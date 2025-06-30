ccc1fde217a21fb34bafb059189144d46d583410
Siim Raudsepp <siimraudsepp@msn.com>
2025-06-30 20:48:38 +0300

    Defined the GUI

--- TemporalTransporter/BlockEntityTemporalTransporter.cs
+++ TemporalTransporter/BlockEntityTemporalTransporter.cs
@@ -1,7 +1,47 @@
-﻿using Vintagestory.API.Common;
+﻿using TemporalTransporter.GUI;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
 
 namespace TemporalTransporter;
 
-public class BlockEntityTemporalTransporter : BlockEntity
+public class BlockEntityTemporalTransporter : BlockEntityOpenableContainer
 {
+    private InventoryGeneric _inventory;
+    private GuiDialogTemporalTransporter? _openDialog;
+
+    public BlockEntityTemporalTransporter(InventoryGeneric inventory)
+    {
+        _inventory = inventory;
+    }
+
+    public BlockEntityTemporalTransporter()
+    {
+    }
+
+
+    public override InventoryBase Inventory => _inventory;
+    public override string InventoryClassName { get; } = "temporaltransporterInv";
+
+    public override bool OnPlayerRightClick(IPlayer byPlayer, BlockSelection blockSel)
+    {
+        var api = byPlayer.Entity.World.Api;
+
+        if (api.Side != EnumAppSide.Client || api is not ICoreClientAPI capi)
+        {
+            return false;
+        }
+
+        _openDialog ??= new GuiDialogTemporalTransporter(Inventory, blockSel.Position, capi);
+
+        _openDialog?.TryOpen();
+
+        return true;
+    }
+
+    public override void Initialize(ICoreAPI api)
+    {
+        _inventory = new InventoryGeneric(1, $"{InventoryClassName}-{Pos}", api);
+        base.Initialize(api);
+    }
 }
---
+++ TemporalTransporter/GUI/GuiDialogTemporalTransporter.cs
@@ -0,0 +1,90 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace TemporalTransporter.GUI;
+
+public class GuiDialogTemporalTransporter : GuiDialogBlockEntity
+{
+    private EnumPosFlag _screenPos;
+
+    public GuiDialogTemporalTransporter(InventoryBase inventory, BlockPos bePos, ICoreClientAPI capi) :
+        base("Temporal Transporter Viewer", inventory, bePos, capi)
+    {
+        capi.Input.RegisterHotKey(
+            "redraw",
+            "Redraw",
+            GlKeys.N,
+            HotkeyType.GUIOrOtherControls
+        );
+        capi.Input.SetHotKeyHandler("redraw", _ => SetupDialog());
+    }
+
+    public override string ToggleKeyCombinationCode => null!;
+
+    private bool SetupDialog()
+    {
+        var stoveBounds = ElementBounds.Fixed(0, 0, 210, 250);
+
+
+        var bgBounds = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding);
+        bgBounds.BothSizing = ElementSizing.FitToChildren;
+        bgBounds.WithChildren(stoveBounds);
+
+        var dialogBounds = ElementStdBounds.AutosizedMainDialog
+                .WithFixedAlignmentOffset(
+                    IsRight(_screenPos) ? -GuiStyle.DialogToScreenPadding : GuiStyle.DialogToScreenPadding, 0)
+                .WithAlignment(IsRight(_screenPos) ? EnumDialogArea.RightMiddle : EnumDialogArea.LeftMiddle)
+            ;
+
+
+        var cookingSlotsSlotBounds = ElementStdBounds.SlotGrid(EnumDialogArea.None, 0, 30 + 45, 4, 1);
+        cookingSlotsSlotBounds.fixedHeight += 10;
+
+        var top = cookingSlotsSlotBounds.fixedHeight + cookingSlotsSlotBounds.fixedY;
+
+        var outputSlotBounds = ElementStdBounds.SlotGrid(EnumDialogArea.None, 153, top, 1, 1);
+
+        SingleComposer = capi.Gui
+            .CreateCompo("temporaltransportergui", dialogBounds)
+            .AddShadedDialogBG(bgBounds)
+            .AddDialogTitleBar(DialogTitle, OnTitleBarClose)
+            .BeginChildElements(bgBounds)
+            .AddDynamicText("", CairoFont.WhiteDetailText(), ElementBounds.Fixed(0, 30, 210, 45), "outputText")
+            .AddItemSlotGrid(Inventory, SendInvPacket, 1, new[] { 2 }, outputSlotBounds, "outputslot")
+            .AddItemSlotGrid(Inventory, SendInvPacket, 4, new[] { 2 }, cookingSlotsSlotBounds, "cookingSlotsSlotBounds")
+            .EndChildElements()
+            .Compose();
+
+        return true;
+    }
+
+    private void SendInvPacket(object packet)
+    {
+        capi.Network.SendBlockEntityPacket(BlockEntityPosition.X, BlockEntityPosition.Y, BlockEntityPosition.Z, packet);
+    }
+
+
+    private void OnTitleBarClose()
+    {
+        TryClose();
+    }
+
+
+    public override void OnGuiOpened()
+    {
+        base.OnGuiOpened();
+
+        _screenPos = GetFreePos("smallblockgui");
+        OccupyPos("smallblockgui", _screenPos);
+        SetupDialog();
+    }
+
+
+    public override void OnGuiClosed()
+    {
+        base.OnGuiClosed();
+
+        FreePos("smallblockgui", _screenPos);
+    }
+}
